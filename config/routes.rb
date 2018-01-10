30fde709491d383e37c8f977772a1553097b3a52
Eva <evaliisagalen@gmail.com>
2018-01-10 14:39:07 -0800

    added sessions controller and sessions new method with form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to:'users#create'
+  get 'users/:id', to: 'users#show', as: 'user'
+
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
